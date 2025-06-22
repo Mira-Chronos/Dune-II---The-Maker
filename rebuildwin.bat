f3db2461abcf3855b6c51164755b883a9c57acef
Mira <mira@legendes-oubliees.org>
2025-06-22 21:32:52 +0200

    Big reorganisation

--- rebuildwin.bat
+++
@@ -1,7 +0,0 @@
-rm -rf build
-mkdir build
-cd build
-cmake .. -G "MinGW Makefiles"
-cmake --build . --target all -- -j 6
-cd ..
-
--- rebuildwinrelease.bat
+++
@@ -1,8 +0,0 @@
-rm -rf Release
-mkdir Release
-cd Release
-cmake -DCMAKE_BUILD_TYPE=Release .. -G "MinGW Makefiles"
-cmake --build . --target all -- -j 6
-cd ..
-
-
